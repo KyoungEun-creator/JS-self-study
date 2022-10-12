73bf53a8615d8657433dc2966475a02cfe1e1c90
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2022-10-13 00:33:56 +0200

    stop event bubbling
    221011 tab에선 black-bg 누르면 모달창 닫아지도록
    stop event bubbling to close modal by clicking only the black background of modal(class name: 'black-bg')

--- 모달창.js
+++ 모달창.js
@@ -4,7 +4,7 @@ document.querySelector('.navbar-toggler').addEventListener('click',function(){
 });
 
 // 로그인 버튼 누르면 모달창 열리기
-// 클릭하면 main.css에 display:block; 해놓은 show-modal 클래스를 추가한다. 
+// 클릭하면 main.css에 display:block; 해놓은 show-modal 클래스를 추가한다. (클래스 탈부착)
 document.querySelector('#login').addEventListener('click',function(){
   document.querySelector('.black-bg').classList.add('show-modal')
 });
@@ -13,6 +13,8 @@ document.querySelector('#close').addEventListener('click',function(){
 });
 
 // black-bg 누르면 모달창 닫히기
-document.querySelector('.black-bg').addEventListener('click',function(){
-  document.querySelector('.black-bg').classList.remove('show-modal')
+document.querySelector('.black-bg').addEventListener('click',function(e){
+  if (e.target == document.querySelector('.black-bg')) {
+    document.querySelector('.black-bg').classList.remove('show-modal')
+  }
 });
